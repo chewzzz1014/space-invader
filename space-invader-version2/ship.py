7f0fcc237d6b70fc75ca8ffbd9d679f68bf66ba8
chewzzz <chewziqing@gmail.com>
2022-04-22 16:46:23 +0800

    Added constructor and methods

--- space-invader-version2/ship.py
+++ space-invader-version2/ship.py
@@ -2,3 +2,16 @@ import pygame
 
 class Ship():
     def __init__(self):
+        self.screen = screen
+
+        # load image and get its rect
+        self.image = pygame.image.load("images/space-invaders.png")
+        self.screen_rect = self.image.get_rect()
+
+        # the starting position of the ship
+        self.rect.centerx = self.screen_rect.centerx
+        self.rect.bottom = self.screen_rect.bottom
+
+    # display the image of ship
+    def blitme(self):
+        self.screen.blit(self.image, self.rect)
